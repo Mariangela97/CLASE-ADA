55081fa34396492765ad04424fb3ebf3cf22557b
Mariangela97 <marypd97@gmail.com>
2016-06-21 16:01:31 -0500

    Create vEB.h

---
+++ vEB.h
@@ -0,0 +1,36 @@
+#ifndef VEB_H
+#define VEB_H
+
+#include "math.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class vEB
+{
+    public:
+        int u;
+        int min;
+        int max;
+        int raiz_u=pow(u,0.5);
+        vector <vEB*> cluster;
+        vEB *summary;
+        vEB(int tam);
+        void vEB_Empty_Tree_insert(vEB* V, int x);
+        void vEB_Tree_Insert(vEB* V, int x);
+        int vEB_Tree_Minimum(vEB* V);
+        int vEB_Tree_Maximum(vEB* V);
+        bool vEB_Tree_Member(vEB* V, int x);
+        int vEB_Tree_Successor(vEB* V, int x);
+        int vEB_Tree_Predecessor(vEB* V, int x);
+        int high(int x);
+        int low(int x);
+        int index(int x,int y);
+
+        virtual ~vEB();
+    protected:
+    private:
+};
+
+#endif // VEB_H
+
